b3f37db08c389b858acb5bf6a40bc1c07cedf72b
avbelousov_1 <whimox@gmail.com>
2023-05-17 17:55:01 +0300

    visualization improvements

--- src/visualize/visualize.py
+++ src/visualize/visualize.py
@@ -2,6 +2,7 @@ from typing import Optional
 from typing import List
 from typing import Dict
 
+import numpy as np
 import pandas
 import seaborn
 
@@ -14,27 +15,60 @@ def draw_single_plot(data: pandas.DataFrame, x_column: str, target_column: str, 
     if title:
         plot.set(title=title)
     if x_label:
-        plot.set(x_label=x_label)
+        plot.set(xlabel=x_label)
     if y_label:
-        plot.set(y_label=y_label)
+        plot.set(ylabel=y_label)
     return plot.get_figure()
 
 
-def draw_multiple_plots(data: Dict[str, pandas.DataFrame], x_column: str, target_column: str,
+def draw_multiline_plot(data: Dict[str, pandas.DataFrame], x_column: str, target_column: str,
+                        legend_title: str, x_scale: str = "linear",
                         title: Optional[str] = None, x_label: Optional[str] = None, y_label: Optional[str] = None):
     merged_df = pandas.concat([
-        df.assign(label=label)
+        df.assign(**{legend_title: label})
         for label, df in data.items()
     ]).reset_index()
 
-    plot = seaborn.lineplot(data=merged_df, x=x_column, y=target_column, hue="label")
+    plot = seaborn.lineplot(data=merged_df, x=x_column, y=target_column, hue=legend_title, style=legend_title)
+    x_ticks = list(merged_df[x_column].unique())
     if title:
         plot.set(title=title)
     if x_label:
-        plot.set(x_label=x_label)
+        plot.set(xlabel=x_label)
     if y_label:
-        plot.set(y_label=y_label)
+        plot.set(ylabel=y_label)
+    
+    plot.set(xscale=x_scale)
+    plot.set(xticks=x_ticks)
+    plot.set(xticklabels=x_ticks)
     return plot.get_figure()
+
+
+def draw_multiline_plots_grid(data: Dict[str, pandas.DataFrame], x_column: str, target_column: str,
+                              grid_column: str, legend_title: str, x_scale: str = "linear",
+                              title: Optional[str] = None, x_label: Optional[str] = None, y_label: Optional[str] = None):
+    merged_df = pandas.concat([
+        df.assign(**{legend_title: label})
+        for label, df in data.items()
+    ]).reset_index()
+
+    plot = seaborn.relplot(data=merged_df, x=x_column, y=target_column, col=grid_column,
+                           hue=legend_title, style=legend_title, kind="line")
+    x_ticks = list(merged_df[x_column].unique())
+    if title:
+        plot.set(title=title)
+    if x_label:
+        plot.set(xlabel=x_label)
+    if y_label:
+        plot.set(ylabel=y_label)
+    
+    plot.set(xscale=x_scale)
+    plot.set(xticks=x_ticks)
+    plot.set(xticklabels=x_ticks)
+    return plot
+
+def normalize_experiment_key(key):
+    return tuple(item if not pandas.isna(item) else None for item in key)
 
 
 def prepare_data_for_plots(experiments_dir: str,
@@ -47,6 +81,6 @@ def prepare_data_for_plots(experiments_dir: str,
         experiments_data = collect_data.filter_duplicate_experiments(experiments_data, key_columns=key_columns,
                                                                      aggregate_columns=aggregate_columns)
 
-    experiments_data_grouped = experiments_data.groupby(key_columns)
-    data_by_experiment = {key: group for key, group in experiments_data_grouped}
+    experiments_data_grouped = experiments_data.groupby(key_columns, dropna=False)
+    data_by_experiment = {normalize_experiment_key(key): group for key, group in experiments_data_grouped}
     return data_by_experiment
